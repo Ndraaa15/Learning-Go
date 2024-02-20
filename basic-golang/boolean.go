121d9c817333d29387e586d8b5554d18ca3e16ee
Gede Indra Adi Brata <112854205+Ndraaa15@users.noreply.github.com>
2024-02-20 11:13:18 +0700

    Delete basic-golang directory

--- basic-golang/boolean.go
+++
@@ -1,8 +0,0 @@
-package main
-
-// import "fmt"
-
-// func main() {
-// 	fmt.Println("Boolean true : ", true)
-// 	fmt.Println("Boolean false : ", false)
-// }
--- basic-golang/main-package.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// "GoLang/helper"
-// "fmt"
-
-func main() {
-	// result := helper.sayHello()
-	// fmt.Println(result)
-}
